2540703f1379e6f83b7c28e24eff4a3511465c22
Austen Blokker <eablokker@gmail.com>
2016-04-15 18:25:07 -0700

    Add getOpacity function for countries layers

--- local_modules/app/public/javascripts/module/heatmap.js
+++ local_modules/app/public/javascripts/module/heatmap.js
@@ -11,23 +11,39 @@ var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/light-v8',
 	center: [0, 35],
-  zoom: 1.5 // starting zoom
+zoom: 1.5 // starting zoom
 });
 
 map.on('load', function () {
-    map.addSource('countries', countries);
-    map.addLayer({
-        'id': 'countries',
-        'type': 'fill',
-        'source': 'countries',
-        'source-layer': 'contour',
-        'layout': {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        'paint': {
-            'fill-color': '#00DAE5',
-            'fill-opacity': 0.5
-        }
-    });
+	map.addSource('countries', countries);
+	map.addLayer({
+		'id': 'countries',
+		'type': 'fill',
+		'source': 'countries',
+		'source-layer': 'contour',
+		'layout': {
+			'line-join': 'round',
+			'line-cap': 'round'
+		},
+		'paint': {
+			'fill-color': '#00DAE5',
+			'fill-opacity': 0.5
+		}
+	});
 });
+
+var topValue = 3275;
+
+function getOpacity(d) {
+	return d > topValue * 0.9 ? 1 :
+	d > topValue * 0.8  ? 0.9 :
+	d > topValue * 0.7  ? 0.8 :
+	d > topValue * 0.6  ? 0.7 :
+	d > topValue * 0.5  ? 0.6 :
+	d > topValue * 0.4  ? 0.5 :
+	d > topValue * 0.3  ? 0.4 :
+	d > topValue * 0.2  ? 0.3 :
+	d > topValue * 0.1  ? 0.2 :
+	d > topValue * 0    ? 0.1 :
+	0;
+}
