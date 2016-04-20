5a84a74f205999f2611b1a848bf1b7162ecf3c88
Paul Shapiro <pshapiro138@gmail.com>
2016-04-20 11:17:48 -0400

    catching nulls before moment is asked for format them as dates

--- local_modules/app/API_data_preparation_controller.js
+++ local_modules/app/API_data_preparation_controller.js
@@ -439,14 +439,14 @@ constructor.prototype.BindDataFor_array_chart = function(urlQuery, callback)
             }
             groupedResults.forEach(function(el, i, arr) 
             {
-                var originalValue = el.label;
-                var displayableVal = originalValue;
-                if (originalValue == null) {
+                var originalVal = el.label;
+                var displayableVal = originalVal;
+                if (originalVal == null) {
                     displayableVal = "(null)"; // null breaks chart but we don't want to lose its data
-                } else if (originalValue === "") {
+                } else if (originalVal === "") {
                     displayableVal = "(not specified)"; // we want to show a label for it rather than it appearing broken by lacking a label
                 } else {
-                    displayableVal = _reverseDataTypeCoersionToMakeFEDisplayableValFrom(originalValue, groupBy_realColumnName, dataSourceDescription);
+                    displayableVal = _reverseDataTypeCoersionToMakeFEDisplayableValFrom(originalVal, groupBy_realColumnName, dataSourceDescription);
                 }
                 el.label = displayableVal;
             });
@@ -728,26 +728,32 @@ function _reverseDataTypeCoersionToMakeFEDisplayableValFrom(originalVal, key, da
     // Perhaps we could do some type-introspection automated formatting later
     // here if needed, but I think generally that kind of thing would be done case-by-case
     // in the template, such as comma-formatting numbers.
-    var raw_rowObjects_coercionScheme = dataSourceDescription.raw_rowObjects_coercionScheme;
-    if (raw_rowObjects_coercionScheme && typeof raw_rowObjects_coercionScheme !== 'undefined') {
-        var coersionSchemeOfKey = raw_rowObjects_coercionScheme["" + key];
-        if (coersionSchemeOfKey && typeof coersionSchemeOfKey !== 'undefined') {
-            var _do = coersionSchemeOfKey.do;
-            if (_do === import_datatypes.Coercion_ops.ToDate) {
-                var dateFormat = null;
-                var opts = coersionSchemeOfKey.opts;
-                if (opts && typeof opts !== 'undefined') {
-                    dateFormat = opts.format;
+    if (originalVal == null) {
+        displayableVal = "(null)"; // null breaks chart but we don't want to lose its data
+    } else if (originalVal === "") {
+        displayableVal = "(not specified)"; // we want to show a label for it rather than it appearing broken by lacking a label
+    } else {
+        var raw_rowObjects_coercionScheme = dataSourceDescription.raw_rowObjects_coercionScheme;
+        if (raw_rowObjects_coercionScheme && typeof raw_rowObjects_coercionScheme !== 'undefined') {
+            var coersionSchemeOfKey = raw_rowObjects_coercionScheme["" + key];
+            if (coersionSchemeOfKey && typeof coersionSchemeOfKey !== 'undefined') {
+                var _do = coersionSchemeOfKey.do;
+                if (_do === import_datatypes.Coercion_ops.ToDate) {
+                    var dateFormat = null;
+                    var opts = coersionSchemeOfKey.opts;
+                    if (opts && typeof opts !== 'undefined') {
+                        dateFormat = opts.format;
+                    }
+                    if (dateFormat == null) {
+                        dateFormat = _defaultFormat;
+                    }
+                    displayableVal = moment(originalVal).format(dateFormat);
+                } else { // nothing to do? (no other types yet)                
                 }
-                if (dateFormat == null) {
-                    dateFormat = _defaultFormat;
-                }
-                displayableVal = moment(originalVal).format(dateFormat);
-            } else { // nothing to do? (no other types yet)                
+            } else { // nothing to do?
             }
         } else { // nothing to do?
         }
-    } else { // nothing to do?
     }
     //
     return displayableVal;
