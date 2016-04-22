368a79ceef9823e15d282865b1a57efb498280bb
Austen Blokker <eablokker@gmail.com>
2016-04-21 17:04:50 -0700

    Get choropleth working with dummy data

--- local_modules/app/public/javascripts/module/choropleth.js
+++ local_modules/app/public/javascripts/module/choropleth.js
@@ -1,49 +1,73 @@
+/**
+ * Initialize variables
+ */
+var layer = 'contour',
+	metric = 'total',
+	numBreaks = 10, // How many layers of opacity values there should be
+	topValue = 100, // This should be the highest "total" value in the data
+	filteruse,
+	breaks = [],
+	opacities = [],
+	names = [];
+
+/**
+ * Generate layer breakpoints, layer names, and opacity values;
+ */
+for (i = 0; i < numBreaks; i++) {
+	breaks[i] = Math.round(topValue * (i / numBreaks)).toString(); // Only works when string
+	opacities[i] = (i / numBreaks);
+	names[i] = 'layer-' + i;
+}
+
+/**
+ * Mapbox access token
+ */
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2NoZW1hIiwiYSI6IjAxcE9MMlkifQ.Kljao5iyXhySu2qYbtw-_A';
 
 /**
- * Load countries data
+ * Create map, set style and centering
  */
-var countries = new mapboxgl.GeoJSONSource({
-	data: '/data/lib/world.geo.json/countries.geo.json'
-});
-
 var map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/mapbox/light-v8',
+	style: 'mapbox://styles/schema/cinavisv200i4bckv2fb6atgp',
 	center: [0, 35],
-zoom: 1.5 // starting zoom
+	zoom: 1.5 // starting zoom
 });
 
+/**
+ * On load map
+ */
 map.on('load', function () {
-	map.addSource('countries', countries);
-	map.addLayer({
-		'id': 'countries',
-		'type': 'fill',
-		'source': 'countries',
-		'source-layer': 'contour',
-		'layout': {
-			'line-join': 'round',
-			'line-cap': 'round'
-		},
-		'paint': {
-			'fill-color': '#00DAE5',
-			'fill-opacity': 0.5
-		}
+
+	/**
+	 * Add source data to map
+	 */
+	map.addSource('countries', {
+		type: 'geojson',
+		data: geoData
 	});
+
+	/**
+	 * Loop through layers, filter countries, and apply opacity
+	 * NOTE: this only works when break values are strings, not integers
+	 */
+	for (i = 0; i < numBreaks; i++) {
+		if (i < numBreaks - 1) {
+			filteruse = ['all', ['>=', metric, breaks[i]], ['<', metric, breaks[i + 1]]];
+		} else {
+			filteruse = ['>=', metric, breaks[i]];
+		}
+
+		map.addLayer({
+			id: names[i],
+			type: 'fill',
+			source: 'countries',
+			'source-layer': layer,
+			filter: filteruse,
+			paint: {
+				'fill-color': '#00DAE5',
+				'fill-opacity': opacities[i]
+			}
+		});
+	}
 });
-
-var topValue = 3275;
-
-function getOpacity(d) {
-	return d > topValue * 0.9 ? 1 :
-	d > topValue * 0.8  ? 0.9 :
-	d > topValue * 0.7  ? 0.8 :
-	d > topValue * 0.6  ? 0.7 :
-	d > topValue * 0.5  ? 0.6 :
-	d > topValue * 0.4  ? 0.5 :
-	d > topValue * 0.3  ? 0.4 :
-	d > topValue * 0.2  ? 0.3 :
-	d > topValue * 0.1  ? 0.2 :
-	d > topValue * 0    ? 0.1 :
-	0;
-}
