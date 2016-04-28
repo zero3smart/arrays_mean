eb5ce7205b4147729156aff7e00cb907484dded5
Austen Blokker <eablokker@gmail.com>
2016-04-28 15:24:26 -0700

    Pin sorting bar to header on scroll on gallery

--- local_modules/app/public/javascripts/module/gallery.js
+++ local_modules/app/public/javascripts/module/gallery.js
@@ -1,3 +1,19 @@
-$(function () {
+$(function() {
 	console.log('gallery loaded');
+
+	/**
+	 * Initialize controller
+	 */
+	var controller = new ScrollMagic.Controller();
+
+	/**
+	 * Set array header pin
+	 */
+	var scene = new ScrollMagic.Scene({
+			offset: - $('.navbar-fixed-top').innerHeight(),
+			triggerElement: '#array-controls'
+		})
+		.triggerHook('onLeave')
+		.setPin('#array-controls')
+		.addTo(controller);
 });
