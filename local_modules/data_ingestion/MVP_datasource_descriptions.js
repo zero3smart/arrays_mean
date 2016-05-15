a21d76753e4137714c41d3a946937bf12a655328
Austen Blokker <eablokker@gmail.com>
2016-05-14 20:52:24 -0700

    Add data and import descriptions for us firms state totals and nasa facilities

--- local_modules/data_ingestion/MVP_datasource_descriptions.js
+++ local_modules/data_ingestion/MVP_datasource_descriptions.js
@@ -454,5 +454,438 @@ exports.Descriptions =
         // }
         // //
         // //
-    }
+    },
+    {
+            filename: "NASA_Labs_Facilities.csv",
+            fileEncoding: "utf8", // the default
+            uid: "nasa_labs_facilities",
+            importRevision: 2,
+            format: import_datatypes.DataSource_formats.CSV,
+            title: "NASA Agency Data on User Facilities",
+            urls: [ "https://catalog.data.gov/dataset/agency-data-on-user-facilities" ],
+            description: "The purpose of the Aerospace Technical Facility Inventory is to facilitate the sharing of specialized capabilities within the aerospace research/engineering community primarily within NASA, but also throughout the nation and the entire world. A second use is to assist in answering questions regarding NASA capabilities for future missions or various alternative scenarios regarding mission support to help the Agency maintain the right set of assets.",
+            //
+            //
+            fn_new_rowPrimaryKeyFromRowObject: function(rowObject, rowIndex)
+            {
+                return "" + rowIndex + "-" + rowObject["ID"]
+            },
+            raw_rowObjects_coercionScheme:
+            {
+                'Record Date': {
+                    do: import_datatypes.Coercion_ops.ToDate,
+                    opts: {
+                        format: "YYYY-MM-DD" // e.g. "2009-03-21"
+                    }
+                },
+                'Last Update': {
+                    do: import_datatypes.Coercion_ops.ToDate,
+                    opts: {
+                        format: "YYYY-MM-DD" // e.g. "2009-03-21"
+                    }
+                }
+            },
+            //
+            //
+            fe_designatedFields: 
+            {
+                objectTitle: "Facility",
+                // originalImageURL: "imgURL_original",
+                // medThumbImageURL: "imgURL_gridThumb"
+            },
+            fe_excludeFields: 
+            [
+                "ID", // not sure if we really want to exclude this
+                //
+                // "imgURL_original", 
+                // "imgURL_gridThumb"
+            ],
+            fe_displayTitleOverrides:
+            { // these are to be tuples - the values must be unique as well
+                // "CuratorApproved": "Curator Approved",
+                // "DateAcquired": "Date Acquired",
+                // "CreditLine": "Credit Line",
+                // "ArtistBio": "Artist Bio",
+                // "MoMANumber": "MoMA Number"
+            },
+            fe_outputInFormat:
+            {
+                'Record Date': {
+                    format: "MMMM Do, YYYY"
+                },
+                'Last Update': {
+                    format: "MMMM Do, YYYY"
+                }
+            },
+            //
+            fe_filters_default:
+            {
+                // "Image": [ "Has image" ]
+            },
+            fe_filters_fieldsNotAvailable:
+            [
+                "Facility", // they're almost exclusively unique
+                "URL Link",
+                "Record Date",
+                "Last Update"
+            ],
+            fe_filters_oneToOneOverrideWithValuesByTitleByFieldName: 
+            {
+                // "Artist Gender": {
+                //     "Male": "Male",
+                //     "Female": "Female",
+                //     "Not Specified": "NULL"
+                // }
+            },
+            fe_filters_valuesToExcludeByOriginalKey:
+            {
+                _all: [ "", null ]
+            },
+            fe_filters_fabricatedFilters:
+            [
+                // {
+                //     title: "Image",
+                //     choices: [
+                //         {
+                //             title: "Has image",
+                //             $match: {
+                //                 "rowParams.imgURL_gridThumb": {
+                //                     $exists: true,
+                //                     $nin: [ "", null ]
+                //                 }
+                //             }
+                //         }
+                //     ]
+                // },
+                // {
+                //     title: "Object Title",
+                //     choices: [
+                //         {
+                //             title: "Has Title",
+                //             $match: {
+                //                 "rowParams.Title": {
+                //                     $exists: true,
+                //                     $nin: [ "", null ]
+                //                 }
+                //             }
+                //         }
+                //     ]
+                // }
+            ],
+            //
+            //
+            fe_galleryItem_htmlForIconFromRowObjWhenMissingImage: function(rowObject)
+            {
+                // var gender = rowObject.rowParams["Artist Gender"];
+                // var iconSpanClass = undefined;
+                // if (typeof gender === 'undefined' || gender == null || gender == "") {
+                //     iconSpanClass = "icon-tile-null";
+                // } else if (gender === "NULL") {
+                //     iconSpanClass = "icon-tile-null";
+                // } else {
+                //     var lowerCasedGender = gender.toLowerCase();
+                //     if (lowerCasedGender == "male" || lowerCasedGender == "female") {
+                //         iconSpanClass = "icon-tile-" + lowerCasedGender;
+                //     } else {
+                //         winston.warn("⚠️  Unrecognized non-NULL lowercased gender: ", gender + ". Defaulting.");
+                //     }
+                // }
+                // //
+                // if (typeof iconSpanClass === 'undefined') { // if for some reason…
+                //     winston.warn("⚠️  Unable to derive icon span class for artist with no image in fe_galleryItem_htmlForIconFromRowObjWhenMissingImage. Using default of 'null' icon.");
+                //     iconSpanClass = "icon-tile-null";
+                // }
+                // //
+                // return '<span class="' + iconSpanClass + ' color-gender"></span>';
+            },
+            //
+            //
+            fe_chart_defaultGroupByColumnName_humanReadable: "Agency",
+            fe_chart_fieldsNotAvailableAsGroupByColumns:
+            [
+                "Facility", // they're almost exclusively unique
+                "URL Link",
+                "Record Date",
+                "Last Update"
+            ],
+            fe_chart_valuesToExcludeByOriginalKey:
+            {
+                _all: [ "", null, "NULL", "(not specified)" ],
+                Classification: [ "(not assigned)" ]
+            },
+            //
+            //
+            fe_choropleth_defaultMapByColumnName_humanReadable: "State",
+            fe_choropleth_fieldsNotAvailableAsMapByColumns:
+            [
+                "Agency",
+                "Center",
+                "Center Search Status",
+                "Facility",
+                "Occupied",
+                "Status",
+                "URL Link",
+                "Record Date",
+                "Last Update",
+                "Address",
+                "City",
+                "State",
+                "ZIP",
+                "Country",
+                "Contact",
+                "Mail Stop",
+                "Phone"
+            ],
+            //
+            //
+            afterImportingAllSources_generate: 
+            [
+                // {
+                //     field: "Artist Gender",
+                //     singular: true, // there is only one gender per artwork's artist
+                //     by: {
+                //         doing: import_processing.Ops.Join,
+                //         matchFn: import_processing.MatchFns.LocalEqualsForeignString,
+                //         findingMatchOnFields: [ "DisplayName" ],
+                //         ofOtherRawSrcUID: "moma_artists",
+                //         andOtherRawSrcImportRevision: 1,
+                //         withLocalField: "Artist",
+                //         obtainingValueFromField: "Code"
+                //     }
+                // },
+                // {
+                //     field: "Country of Origin",
+                //     singular: true,
+                //     by: {
+                //         doing: import_processing.Ops.Join, 
+                //         matchFn: import_processing.MatchFns.LocalContainsForeignString, // look for Demonym in ArtistBio, rather than ArtistBio in Demonym
+                //         findingMatchOnFields: [ "Demonym 1", "Demonym 2", "Demonym 3" ],
+                //         ofOtherRawSrcUID: "countries_to_demonyms",
+                //         andOtherRawSrcImportRevision: 1,
+                //         withLocalField: "ArtistBio", 
+                //         obtainingValueFromField: "Name"
+                //     }
+                // }
+            ],
+            //
+            //
+            afterImportingAllSources_generateByScraping:
+            [
+                // {
+                //     htmlSourceAtURLInField: "URL",
+                //     imageSrcSetInSelector: "img.sov-hero__image-container__image@srcset",
+                //     prependToImageURLs: "http://www.moma.org", // since the urls are like "/media/…", not "http://…/media/…"
+                //     useAndHostSrcSetSizeByField: {
+                //         "imgURL_original": {
+                //             size: "2000w"
+                //         },
+                //         "imgURL_gridThumb": {
+                //             size: "640w"
+                //         }
+                //     }
+                // }
+            ]
+            // //
+            // //
+            // // This is implemented but currently not used (it was built for scraping)
+            // afterGeneratingProcessedRowObjects_setupBefore_eachRowFn: function(appCtx, eachCtx, cb)
+            // {
+            //     // Setup each ctx, such as the batch operation
+            //     cb(null);
+            // },
+            // //
+            // afterGeneratingProcessedRowObjects_eachRowFns:
+            // [
+            //     function(appCtx, eachCtx, rowDoc, cb)
+            //     {
+            //         // console.log("A row", rowDoc)
+            //         // perform derivations and add update operations to batch operation in eachCtx
+            //         cb(null);
+            //     }
+            // ],
+            // //
+            // afterGeneratingProcessedRowObjects_afterIterating_eachRowFn: function(appCtx, eachCtx, cb)
+            // {
+            //     // Finished iterating … execute the batch operation
+            //     cb(null);
+            // }
+            // //
+            // //
+        },
+    //
+    // Production - Visible - Total Workplaces by State 2013
+    // {
+    //     filename: "us_state_totals_2013.csv",
+    //     fileEncoding: "utf8", // the default
+    //     uid: "total_workplaces_by_state_2013",
+    //     importRevision: 1,
+    //     format: import_datatypes.DataSource_formats.CSV,
+    //     title: "U.S. Firms & Establishments, 2013",
+    //     urls: [ "http://www.census.gov/econ/susb/" ],
+    //     description: "Number of Firms, Number of Establishments, Employment, and Annual Payroll by Enterprise Employment Size for the United States and States, Totals: 2013",
+    //     //
+    //     //
+    //     fn_new_rowPrimaryKeyFromRowObject: function(rowObject, rowIndex)
+    //     {
+    //         return "" + rowIndex + "-" + rowObject["ID"]
+    //     },
+    //     // raw_rowObjects_coercionScheme:
+    //     // {
+    //     //     BeginDate: {
+    //     //         do: import_datatypes.Coercion_ops.ToDate,
+    //     //         opts: import_datatypes.Coercion_optionsPacks.ToDate.FourDigitYearOnly
+    //     //     },
+    //     //     EndDate: {
+    //     //         do: import_datatypes.Coercion_ops.ToDate,
+    //     //         opts: import_datatypes.Coercion_optionsPacks.ToDate.FourDigitYearOnly
+    //     //     }
+    //     // },
+    //     //
+    //     //
+    //     // afterImportingAllSources_generate:
+    //     // [
+    //     //     {
+    //     //         field: "Country of Origin",
+    //     //         singular: true,
+    //     //         by: {
+    //     //             doing: import_processing.Ops.Join, 
+    //     //             matchFn: import_processing.MatchFns.LocalContainsForeignString, // look for Demonym in Nationality, rather than Nationality in Demonym
+    //     //             findingMatchOnFields: [ "Demonym 1", "Demonym 2", "Demonym 3" ],
+    //     //             ofOtherRawSrcUID: "countries_to_demonyms",
+    //     //             andOtherRawSrcImportRevision: 1,
+    //     //             withLocalField: "Nationality", 
+    //     //             obtainingValueFromField: "Name"
+    //     //         }
+    //     //     },
+    //     //     {
+    //     //         field: "Artworks",
+    //     //         singular: false,
+    //     //         relationship: true, // obtaining the _id instead of a field value, and will be hydrated with
+    //     //         // associated objects on API data prep instead of being sent along as primitive values
+    //     //         by: {
+    //     //             doing: import_processing.Ops.Join,
+    //     //             matchFn: import_processing.MatchFns.LocalEqualsForeignString,
+    //     //             findingMatchOnFields: [ "Artist" ],
+    //     //             ofOtherRawSrcUID: "moma_artworks",
+    //     //             andOtherRawSrcImportRevision: 2,
+    //     //             withLocalField: "DisplayName"
+    //     //             // note we do not obtain a value from a field since this is a relationship-forming join
+    //     //             // and a flag here saying "obtainingRelationship=true" would be redundant
+    //     //         }
+    //     //     }
+    //     // ],
+    //     //
+    //     //
+    //     fe_designatedFields: 
+    //     {
+    //         objectTitle: "Geographic Area Description",
+    //         originalImageURL: null, // not strictly necessary to define as null but done for explicitness
+    //         medThumbImageURL: null // not strictly necessary to define as null but done for explicitness
+    //     },
+    //     fe_excludeFields: 
+    //     [
+    //         "ID", // not sure if we really want to exclude this
+    //         "FIPS Code",
+    //         "Employment Noise Flag",
+    //         "Annual Payroll Noise Flag"
+    //     ],
+    //     fe_displayTitleOverrides:
+    //     { // these are to be tuples - the values must be unique as well
+    //         // "Code" : "Gender",
+    //         // "DisplayDate" : "Bio",
+    //         // "BeginDate" : "Date of Birth",
+    //         // "EndDate" : "Date of Death"
+    //     },
+        
+    //     fe_filters_fieldsNotAvailable:
+    //     [
+    //         "ID", // because they're effectively unique
+    //         "FIPS Code",
+    //         "Number of Firms",
+    //         "Number of Establishments",
+    //         "Employment",
+    //         "Employment Noise Flag",
+    //         "Annual Payroll",
+    //         "Annual Payroll Noise Flag"
+    //     ],
+    //     // fe_filters_oneToOneOverrideWithValuesByTitleByFieldName: 
+    //     // {
+    //     //     "Gender": {
+    //     //         "Male": "Male",
+    //     //         "Female": "Female",
+    //     //         "Not Specified": "NULL"
+    //     //     }
+    //     // },
+    //     //
+    //     // fe_filters_valuesToExcludeByOriginalKey:
+    //     // {
+    //     //     _all: [ "", null ],
+    //     //     "Nationality" : [ "NULL", "Nationality unknown", "nationality unknown", "Nationality Unknown" ]
+    //     // },
+    //     //
+    //     fe_chart_defaultGroupByColumnName_humanReadable: "Geographic Area Description",
+    //     fe_chart_fieldsNotAvailableAsGroupByColumns:
+    //     [
+    //         "ID", // because they're effectively unique
+    //         "FIPS Code"
+    //     ],
+    //     // fe_chart_valuesToExcludeByOriginalKey:
+    //     // {
+    //     //     _all: [ "", null, "NULL", "(not specified)" ],
+    //     //     Nationality: [ "Nationality unknown", "nationality unknown", "Nationality Unknown" ]
+    //     // },
+    //     //
+    //     //
+    //     fe_choropleth_defaultMapByColumnName_humanReadable: "Geographic Area Description",
+    //     fe_choropleth_fieldsNotAvailableAsMapByColumns:
+    //     [
+    //         "ID", // because they're effectively unique
+    //         "FIPS Code",
+    //         "Number of Firms",
+    //         "Number of Establishments",
+    //         "Employment",
+    //         "Employment Noise Flag",
+    //         "Annual Payroll",
+    //         "Annual Payroll Noise Flag"
+    //     ],
+    //     //
+    //     //
+    //     // fe_galleryItem_htmlForIconFromRowObjWhenMissingImage: function(rowObject)
+    //     // {
+    //     //     var gender = rowObject.rowParams["Code"];
+    //     //     var iconSpanClass = undefined;
+    //     //     if (typeof gender === 'undefined' || gender == null || gender == "") {
+    //     //         iconSpanClass = "icon-tile-null";
+    //     //     } else if (gender === "NULL") {
+    //     //         iconSpanClass = "icon-tile-null";
+    //     //     } else {
+    //     //         var lowerCasedGender = gender.toLowerCase();
+    //     //         if (lowerCasedGender == "male" || lowerCasedGender == "female") {
+    //     //             iconSpanClass = "icon-tile-" + lowerCasedGender;
+    //     //         } else {
+    //     //             winston.warn("⚠️  Unrecognized non-NULL lowercased gender: ", gender + ". Defaulting.");
+    //     //         }
+    //     //     }
+    //     //     //
+    //     //     if (typeof iconSpanClass === 'undefined') { // if for some reason…
+    //     //         winston.warn("⚠️  Unable to derive icon span class for artist with no image in fe_galleryItem_htmlForIconFromRowObjWhenMissingImage. Using default of 'null' icon.");
+    //     //         iconSpanClass = "icon-tile-null";
+    //     //     }
+    //     //     //
+    //     //     return '<span class="' + iconSpanClass + ' color-gender"></span>';
+    //     // },
+    //     //
+    //     // fe_objectShow_customHTMLOverrideFnsByColumnName:
+    //     // {
+    //     //     "Artworks": function(rowObject, eachValue)
+    //     //     {
+    //     //         var relationshipObjectShowLink = "/array/" + eachValue.srcDocPKey + "/" + eachValue._id;
+    //     //         var openingTag = '<a href="' + relationshipObjectShowLink + '" class="color-brand">';
+    //     //         var tagContent = eachValue.rowParams.Title;
+    //     //         var closingTag = '</a>';
+    //     //         //
+    //     //         return openingTag + tagContent + closingTag;
+    //     //     }
+    //     // }
+    // }
 ]
