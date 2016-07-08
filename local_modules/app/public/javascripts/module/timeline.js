1aec8fa471be3d349fb4fad76f7abe10667572ad
Austen Blokker <eablokker@gmail.com>
2016-07-07 17:39:47 -0700

    Improve timeline tooltip display auto width

--- local_modules/app/public/javascripts/module/timeline.js
+++ local_modules/app/public/javascripts/module/timeline.js
@@ -1,4 +1,13 @@
 /**
+ * Tooltip
+ */
+var $tooltip = $('<div class="global-tooltip"></div>').appendTo('body');
+
+var $tooltipKey = $('<span class="tooltip-key"></span>').appendTo($tooltip);
+
+var $tooltipValue = $('<span class="tooltip-value"></span>').appendTo($tooltip);
+
+/**
  * Tooltip behavior on mouse hover
  */
 var $timelineGroupItem = $('.timeline-group-item');
@@ -6,14 +15,15 @@ var $timelineGroupItem = $('.timeline-group-item');
 $timelineGroupItem.each(function(i) {
 	var $this = $(this);
 
-	var $tooltip = $this.find('.global-tooltip');
-
-	$this.on('mouseover', function(d) {
+	$this.on('mouseover', function() {
+		console.log($this.data('tooltip-key'));
+		$tooltipKey.html($this.data('tooltip-key'));
+		$tooltipValue.html($this.data('tooltip-value'));
 		$tooltip.css('display', 'block');
 	});
 
 	$this.on('mousemove', function() {
-		$tooltip.css('top', (event.pageY - 15 - $this.offset().top)+'px').css('left', (event.pageX - $this.offset().left)+'px');
+		$tooltip.css('top', (event.pageY - 15)+'px').css('left', (event.pageX)+'px');
 	});
 
 	$this.on('mouseout', function() {
