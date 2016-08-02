6a8f037d516cd3a734e2632c2ae19e214a49c307
Austen Blokker <eablokker@gmail.com>
2016-08-02 11:06:44 -0700

    Change source URL on Seattle Public Library array

--- local_modules/data_ingestion/datasource_descriptions/transcript_schema.js
+++ local_modules/data_ingestion/datasource_descriptions/transcript_schema.js
@@ -12,7 +12,7 @@ exports.Descriptions =
             importRevision: 1,
             title: "Seattle Public Library Special Collections",
             brandColor: "#6699CC",
-            urls: [ "http://cdm16118.contentdm.oclc.org/cdm/landingpage/collection/p16118coll10" ],
+            urls: [ "http://www.spl.org/library-collection/special-collections" ],
             description: "Founded by Herman Horowitz in 1924, The Jewish Transcript documents the daily life of the Jewish community in Seattle as well as local and international events from the 1920's to present day.",
             raw_rowObjects_coercionScheme:
             {
