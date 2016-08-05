d2889f70613d3cdaa21c87320653ecb0cdc17146
Alexandr Zelenin <zeleniy@Alexandrs-MacBook-Pro.local>
2016-08-05 19:01:53 +0300

    Add navigation chart for time series

--- local_modules/app/public/javascripts/module/linechart/chart.js
+++ local_modules/app/public/javascripts/module/linechart/chart.js
@@ -10,127 +10,13 @@ linechart.chart = function(data) {
      */
     this._data = data;
     /**
-     * Chart container.
-     * @private
-     * @member {Selection}
+     * 
      */
-    this._container = undefined;
+    this._viewport = new linechart.viewport(this._data);
     /**
-     * Chart canvas width.
-     * @private
-     * @member {Number}
+     * 
      */
-    this._innerWitdh = undefined;
-    /**
-     * Chart canvas height.
-     * @private
-     * @member {Number}
-     */
-    this._innerHeight = undefined;
-    /**
-     * Chart SVG width.
-     * @private
-     * @member {Number}
-     */
-    this._outerWitdh = undefined;
-    /**
-     * Chart SVG height.
-     * @private
-     * @member {Number}
-     */
-    this._outerHeight = undefined;
-    /**
-     * Chart SVG element.
-     * @private
-     * @member {Selection}
-     */
-    this._svg = undefined;
-    /**
-     * Chart main g element.
-     * @private
-     * @member {Selection}
-     */
-    this._canvas = undefined;
-    /**
-     * Chart x scale function.
-     * @private
-     * @member {Function}
-     */
-    this._xScale = d3.time.scale();
-    /**
-     * Chart y scale function.
-     * @private
-     * @member {Function}
-     */
-    this._yScale = d3.scale.linear();
-    /**
-     * Chart x axis.
-     * @private
-     * @member {Function}
-     */
-    this._xAxis = d3.svg.axis()
-        .scale(this._xScale)
-        .orient('bottom');
-    /**
-     * Chart y axis.
-     * @private
-     * @member {Function}
-     */
-    this._yAxis = d3.svg.axis()
-        .scale(this._yScale)
-        .orient('left');
-    /**
-     * Chart x axis container.
-     * @private
-     * @member {Selection}
-     */
-    this._xAxisContainer = undefined;
-    /**
-     * Chart y axis container.
-     * @private
-     * @member {Selection}
-     */
-    this._yAxisContainer = undefined;
-    /**
-     * Lines color set.
-     * @private
-     * @member {String[]}
-     */
-    this._colors = d3.scale.category10().range();
-    /*
-     * Stash reference to this object.
-     */
-    var self = this;
-    /**
-     * Line generator.
-     * @private
-     * @member {Function}
-     */
-    this._lineGenerator = d3.svg.line()
-        .x(function(d) {
-            return self._xScale(d.year);
-        }).y(function(d) {
-            return self._yScale(d.count);
-        });
-    /**
-     * Chart margins.
-     * @private
-     * @member {Object}
-     */
-    this._margin = {
-        top : 25,
-        right : 0,
-        bottom : 20,
-        left : 45
-    };
-    /*
-     * Set up window resize event handler.
-     */
-    var self = this;
-    window.onresize = function() {
-        self.resize();
-        self.update();
-    };
+    this._navigation = new linechart.navigation(this._data);
 }
 
 
@@ -151,51 +37,18 @@ linechart.chart.prototype.render = function(selector) {
     if (this._container.size() === 0) {
         throw new Error('Cannot find HTML element by "' + selector + '" selector');
     }
+
+    this._container.style('height', '400px');
+
+    var container = this._container.append('div')
+        .style('height', '75%');
+    this._viewport.render(container);
+
+    container = this._container.append('div')
+        .style('height', '25%');
+    this._navigation.render(container);
     /*
-     * Append SVG element.
-     */
-    this._svg = this._container.append('svg');
-    /*
-     * Append chart canvas.
-     */
-    this._canvas = this._svg.append('g')
-        .attr('transform', 'translate(' + this._margin.left + ', ' + this._margin.top + ')');
-    /*
-     * Append x axis container.
-     */
-    this._xAxisContainer = this._canvas.append('g')
-        .attr('class', 'axis x-axis');
-    /*
-     * Append y axis container.
-     */
-    this._yAxisContainer = this._canvas.append('g')
-        .attr('class', 'axis y-axis');
-    /*
-     * Append x lable container
-     */
-    this._xLabelContainer = this._svg.append('g')
-        .attr('class', 'label x-label')
-        .attr('text-anchor', 'middle')
-        .append('text')
-        .attr('letter-spacing', 2);
-    /*
-     * Append y label container.
-     */
-    this._yLabelContainer = this._svg.append('g')
-        .attr('class', 'label y-label')
-        .attr('text-anchor', 'middle')
-        .append('text')
-        .attr('letter-spacing', 2);
-    /*
-     * Render empty line.
-     */
-    this._line = this._canvas.selectAll('path.line')
-        .data(this._data)
-        .enter()
-        .append('path')
-        .attr('class', 'line');
-    /*
-     * Set up chart dimension.
+     * Set up chart size.
      */
     this.resize();
     /*
@@ -213,41 +66,9 @@ linechart.chart.prototype.render = function(selector) {
  * @returns {linechart.chart}
  */
 linechart.chart.prototype.resize = function() {
-    /*
-     * Get container dimensions.
-     */
-    var dimension = this._container.node().getBoundingClientRect();
-    /*
-     * Set up chart outer and inner width and height.
-     */
-    this._outerWidth = dimension.width;
-    this._innerWidth = this._outerWidth - this._margin.left - this._margin.right;
-    this._outerHeight = 400;
-    this._innerHeight = this._outerHeight - this._margin.top - this._margin.bottom;
-    /*
-     * Recalculate inner width and shift canvas again.
-     * This is necessary if in code above (mobile check) was changes.
-     */
-    this._canvas.attr('transform', 'translate(' + this._margin.left + ', ' + this._margin.top + ')');
-    this._innerWidth = this._outerWidth - this._margin.left - this._margin.right;
-    /*
-     * Configure scale functions.
-     */
-    this._xScale.range([0, this._innerWidth]);
-    this._yScale.range([this._innerHeight, 0]);
-    /*
-     * Append SVG element.
-     */
-    this._svg.attr('width', this._outerWidth)
-        .attr('height', this._outerHeight);
-    /*
-     * Update grid.
-     */
-    this._yAxis.tickSize(- this._innerWidth, 0);
-    /*
-     * Move x axis corresponding with chart height.
-     */
-    this._xAxisContainer.attr('transform', 'translate(0, ' + this._innerHeight + ')');
+
+    this._viewport.resize();
+    this._navigation.resize();
 
     return this;
 };
@@ -264,39 +85,9 @@ linechart.chart.prototype.update = function(data) {
      * Use current data if not provided.
      */
     data = data || this._data;
-    /*
-     * Stash reference to this object.
-     */
-    var self = this;
-    /*
-     * Get x extent.
-     */
-    this._xDomain = d3.extent(this._data[0], function(d) {
-        return d.year;
-    });
-    /*
-     * Get y extent.
-     */
-    this._yDomain = [0, d3.max(this._data[0], function(d) {
-        return d.count;
-    })];
-    /*
-     * Update scale functions.
-     */
-    this._xScale.domain(this._xDomain);
-    this._yScale.domain(this._yDomain);
-    /*
-     * Update chart axes.
-     */
-    this._xAxisContainer.call(this._xAxis);
-    this._yAxisContainer.call(this._yAxis);
-    /*
-     * Update lines.
-     */
-    this._line.attr("d", this._lineGenerator)
-        .style('stroke', function(d, i) {
-            return self._colors[i];
-        });
+
+    this._viewport.update(data);
+    this._navigation.update(data);
 
     return this;
 };
