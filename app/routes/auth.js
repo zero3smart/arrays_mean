84653a975d0defd321203f7a8c8e2d90097371e2
Thomas Lee <lucky.thomaslee@gmail.com>
2016-08-23 07:18:37 +0800

    Auth0 alpha version

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,7 +4,7 @@ module.exports = function(context) {
     var env = {
         AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
         AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-        AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+        AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:' + process.env.PORT + '/callback'
     };
 
     var app = context.app;
@@ -21,6 +21,6 @@ module.exports = function(context) {
 
     app.get('/callback', passport.authenticate('auth0', { failureRedirect: '/' }), function(req, res)
     {
-        res.redirect(req.session.returnTo || '/user/profile');
+        res.redirect(req.session.returnTo || '/array/create');
     });
 }
