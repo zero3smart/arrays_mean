aba3e38260c09d9b1834d660fd7626e6e5c26afa
Thomas Lee <lucky.thomaslee@gmail.com>
2016-08-23 01:06:01 +0800

    Modified the example.env

--- app/app.js
+++ app/app.js
@@ -16,35 +16,13 @@ require('dotenv').config(dotenv_config);
 var path = require('path');
 var express = require('express');
 var winston = require('winston');
-var Auth0Strategy = require('passport-auth0');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
 
 // This will configure Passport to use Auth0
-var strategy = new Auth0Strategy({
-    domain:       process.env.AUTH0_DOMAIN,
-    clientID:     process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:9080/callback'
-}, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-});
-
-passport.use(strategy);
-
-// we can use this section to keep a smaller payload
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
+var strategy = require('./setup-passport');
 
 var app = express();
 //
@@ -79,7 +57,7 @@ var helmet = require('helmet');
 app.use(helmet.xframe());
 app.use(cookieParser());
 app.use(session({
-    secret: 'asdfwfwefosdiofjo10293',
+    secret: 'djU18Hkl3V7Jb7p96l3v7MA31PZX7dwS4_y6J_uURxf03Ed-32g6YvHrf-sHw2d5P',
     resave: true,
     saveUninitialized: true
 }));
--- app/setup-passport.js
+++ app/setup-passport.js
@@ -2,10 +2,10 @@ var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
 
 var strategy = new Auth0Strategy({
-    domain:       'YOUR_NAMESPACE',
-    clientID:     'YOUR_CLIENT_ID',
-    clientSecret: 'YOUR_CLIENT_SECRET',
-    callbackURL:  '/callback'
+    domain:       process.env.AUTH0_DOMAIN,
+    clientID:     process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:9080/callback'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
