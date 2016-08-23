ce0725e256a8b80bf162cb54546b5f6813f61caa
Thomas Lee <lucky.thomaslee@gmail.com>
2016-08-23 21:00:31 +0800

    Fix the auth0 callback url

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,7 +4,7 @@ module.exports = function(context) {
     var env = {
         AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
         AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-        AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:' + process.env.PORT + '/callback'
+        AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:' + (process.env.PORT||9080) + '/callback'
     };
 
     var app = context.app;
