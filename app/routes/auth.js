4d50088168999e2a163542382e970cbfdf978fc4
Thomas Lee <lucky.thomaslee@gmail.com>
2016-09-28 00:35:17 +0800

    Adding the user's role

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var express = require('express');
 var router = express.Router();
+var requireRole = require('../requireRole');
 
 router.get('/login', function (req, res) {
     res.render('auth/login', {env: process.env});
@@ -11,8 +12,18 @@ router.get('/logout', function (req, res) {
     res.redirect('/');
 });
 
-router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/'}), function (req, res) {
+router.get('/auth/callback', passport.authenticate('auth0', {failureRedirect: '/auth/failure'}), function (req, res) {
+    console.log(req.user.Profile._json);
     res.redirect(req.session.returnTo || '/array');
 });
+
+router.get('/admin', requireRole('admin'), function (req, res) {
+    res.render('auth/admin');
+});
+
+router.get('/unauthorized', function(req, res) {
+    res.render('auth/unauthorized', {env: env});
+});
+
 
 module.exports = router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -60,7 +60,7 @@ var _mountRoutes_errorHandling = function (app) {
 var _mountRoutes_endPoints = function (app) {
     // View endpoints
     app.use('/', require('./homepage'));
-    app.use('/auth', require('./auth'));
+    app.use('/', require('./auth'));
     app.use('/array', require('./array'));
     app.use('/team', require('./team'));
     app.use('/s', require('./shared_pages'));
