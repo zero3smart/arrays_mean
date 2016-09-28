277b103f592ba21c6c4a471d0a0518776712cb60
Andre Lindquist <andre@schemadesign.com>
2016-09-27 17:43:47 -0700

    Change tooltip to show 'records' instead of 'characters' & disable linegraph in VMS array

--- app/datasources/descriptions/vms_cumulative.js
+++ app/datasources/descriptions/vms_cumulative.js
@@ -7,15 +7,15 @@ var import_datatypes = require('../utils/import_datatypes');
 exports.Descriptions =
     [
         {
-            filename: "VMS_Cumulative_v6.csv",
+            filename: "VMS_Cumulative_v2.csv",
             fileEncoding: "utf8", // the default
-            uid: "vms survey",
+            uid: "vms_survey",
             importRevision: 2,
             format: import_datatypes.DataSource_formats.CSV,
             title: "Visitor Motivation Survey",
             brandColor: "#03A678",
             urls: [ "--" ],
-            description: "This data describes the motivations behind five different categories of users who have visisted museum websites.  Currently, this five different museums are represented in this data.",
+            description: "This data describes the motivations behind five different categories of users who have visisted museum websites.  Currently, five different museums are represented in this data.",
             fe_listed: true,
             fe_displayTitleOverrides: {
 
@@ -28,7 +28,15 @@ exports.Descriptions =
             },
             raw_rowObjects_coercionScheme:
             {
-
+                'Date': {  //Use with format: "MM/DD/YYYY"
+                // 'Date': {
+                    do: import_datatypes.Coercion_ops.ToDate,
+                    opts: {
+                        // format: "MM/DD/YY" //LineGraph: results in a tick for every single day on x-axis
+                        // format: 'YYYY'  //LineGraph: results in only 2 ticks on x-axis
+                        format: 'YYYYMMDD'  //LineGraph: results in blank x-axis
+                    }
+                }
             },
             //
             //
@@ -43,7 +51,7 @@ exports.Descriptions =
                 scatterplot: true,
                 timeline: false,
                 wordCloud: false,
-                lineGraph: true
+                lineGraph: false
             },
             fe_default_view: 'chart',
             fe_excludeFields:
@@ -74,8 +82,9 @@ exports.Descriptions =
             fe_chart_defaultGroupByColumnName_humanReadable: "Motivation",
             fe_chart_fieldsNotAvailableAsGroupByColumns:
             [
-                    "Minute_Index",
+                    "Minute Index",
                     "Date",
+                    "Date2",  //Remove later after finding solution to the date format issue
                     "Hour",
                     "Minute",
                     "Sessions",
@@ -92,26 +101,56 @@ exports.Descriptions =
                 'Object Title' : 'Motivation'
             },
             fe_scatterplot_fieldsNotAvailable: [
-                "Pageviews",
+                "Institution",
+                "Date",
+                "Date2",  //delete after reworking CSV
+                "Hour",
+                "Minute",
+                "Motivation",
+                "Session Duration",
+                "Pages Per Session",
+                "Landing Page",
+                "User Type", 
+                "Traffic Type",
+                "Operating System",
                 "Minute Index",
+                "City",
+                "Region",
+                "Country"
             ],
             fe_scatterplot_defaults: {
-                xAxisField: 'Motivation',
-                yAxisField: 'Pageviews'
+                xAxisField: 'Pageviews',
+                yAxisField: 'Sessions'
             },
-            fe_lineGraph_defaultGroupByColumnName_humanReadable: "Motivation",
+            ///////////
+
+            fe_timeline_defaultSortByColumnName_humanReadable: "Date",
+            fe_lineGraph_defaultGroupByColumnName_humanReadable: "Date",
+            fe_lineGraph_keywordGroupBy: "Motivation",
             fe_lineGraph_fieldsNotAvailableAsGroupByColumns:
                 [
+                    "Institution",
+                    "Motivation",
+                    "Sessions",
+                    "Traffic Type",
                     "Minute Index",
-                    "Date",
                     "Hour",
                     "Minute",
-                    "Sessions",
-                    "Session Duration",
-                    "Pageviews",
-                    "Pages Session",
                     "Landing Page",
                 ],
-            
+            fe_lineGraph_defaultAggregateByColumnName_humanReadable: "Pageviews",
+            fe_lineGraph_aggregateByColumnName_numberOfRecords_notAvailable: true,
+            fe_lineGraph_keywordLineColors: {
+                'Explorer': '#33B1B1',
+                'Facilitator': '#9533F8',
+                'Professional': '#FEB600',
+                'Recharger': '#99D8D8',
+                'Seeker': '#4D8DFF',
+            },
+            fe_lineGraph_mapping_dataSource_pKey: "vms_survey-r2",
+            fe_lineGraph_mapping_dataSource_fields: {
+                Year: "Date"
+            }
+
         }
     ];
