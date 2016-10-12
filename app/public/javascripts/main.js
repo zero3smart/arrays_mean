1b9a5e51f12567a81383477de390846258311f97
Shu Han Ng <nsh1026@uw.edu>
2016-10-12 14:02:13 -0700

    Mixpanel fix

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -1,6 +1,13 @@
 $(window).load(function() {
 
     trackEvent("page load");
+
+
+     trackEvent('page viewed', {
+       'page name' : document.title,
+       'url' : window.location.pathname
+     });
+
     
     /**
      * Add class to body to prevent weird page width transitions
