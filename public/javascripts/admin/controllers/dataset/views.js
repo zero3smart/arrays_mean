466042dcad23a2c9e466f847a3c0489b7e30f353
Susanna <nsh1026@uw.edu>
2016-11-26 22:51:55 -0800

    disallow duplicate key and reset function

--- public/javascripts/admin/controllers/dataset/views.js
+++ public/javascripts/admin/controllers/dataset/views.js
@@ -23,7 +23,8 @@ angular.module('arraysApp')
 	                    }
 	                })
 	                    .then(function (savedDataset) {
-	                        // $scope.$parent.$parent.dataset = savedDataset;
+	                       $scope.$parent.$parent.dataset = savedDataset;
+                            $scope.viewsForm.$setDirty();
 	                    }, function () {
 	                        console.log('You cancelled the dialog.');
 	                    });
@@ -33,12 +34,58 @@ angular.module('arraysApp')
             	})
             };
 
+
+            $scope.reset = function () {
+                $scope.$parent.$parent.dataset = angular.copy(dataset);
+                $scope.viewsForm.$setPristine();
+            };
+
+            $scope.submitForm = function (isValid) {
+                if (isValid) {
+                    // var finalizedDataset = angular.copy($scope.$parent.$parent.dataset);
+                    // finalizedDataset.fn_new_rowPrimaryKeyFromRowObject = $filter('dotless')($scope.data.primaryKey);
+                    // delete finalizedDataset.firstRecord;
+                    // delete finalizedDataset.colNames;
+
+                    // console.log(finalizedDataset);
+
+                    // DatasetService.save(finalizedDataset)
+                    //     .then(function (id) {
+                    //         $mdToast.show(
+                    //             $mdToast.simple()
+                    //                 .textContent('Dataset updated successfully!')
+                    //                 .position('top right')
+                    //                 .hideDelay(3000)
+                    //         );
+
+                    //         $state.transitionTo('admin.dataset.views', {id: id}, {
+                    //             reload: true,
+                    //             inherit: false,
+                    //             notify: true
+                    //         });
+                    //     }, function (error) {
+                    //         $mdToast.show(
+                    //             $mdToast.simple()
+                    //                 .textContent(error)
+                    //                 .position('top right')
+                    //                 .hideDelay(5000)
+                    //         );
+                    //     });
+                }
+            }
+
+
+
+
+
+
             function ViewDialogController($scope, $mdDialog, $filter, viewName,viewDisplayName,dataset,viewSetting) {
 
                 $scope.viewName = viewName;
                 $scope.viewDisplayName = viewDisplayName;
 				$scope.viewSetting = viewSetting;
                 $scope.isDefault = false;
+          
 
                 $scope.availableForDuration = [ "Decade", "Year", "Month", "Day"];
 
@@ -65,50 +112,15 @@ angular.module('arraysApp')
                     $scope.dataset = angular.copy(dataset);
                     if ($scope.dataset.fe_views.default_view == viewName) {
                         $scope.isDefault = true;
-                    } 
+                    } else {
+                        $scope.isDefault = false;
+                    }
+                    $scope.data = {};
+                    for (var i = 0; i < viewSetting.length; i++) {
+                        var setting_name = viewSetting[i].name;
+                        $scope.data[setting_name] =  $scope.dataset.fe_views.views[viewName][setting_name];
 
-                    // $scope.data = {};
-
-                    // // General
-                    // if (!$scope.dataset.fe_designatedFields) $scope.dataset.fe_designatedFields = {};
-                    // for (var key in $scope.dataset.fe_designatedFields) {
-                    //     if ($scope.dataset.fe_designatedFields[key] == $scope.finalizedFieldName) {
-                    //         $scope.data.designatedField = key;
-                    //         break;
-                    //     }
-                    // }
-
-                    // if (!$scope.dataset.fe_fieldDisplayOrder) $scope.dataset.fe_fieldDisplayOrder = [];
-                    // var index = $scope.dataset.fe_fieldDisplayOrder.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.data.displayOrder = index;
-
-                    // // Filter
-                    // if (!$scope.dataset.fe_filters.fieldsNotAvailable) $scope.dataset.fe_filters.fieldsNotAvailable = [];
-                    // $scope.data.filterNotAvailable = $scope.dataset.fe_filters.fieldsNotAvailable.indexOf($scope.finalizedFieldName) != -1;
-
-                    // if (!$scope.dataset.fe_filters.fieldsCommaSeparatedAsIndividual) $scope.dataset.fe_filters.fieldsCommaSeparatedAsIndividual = [];
-                    // $scope.data.commaSeparatedAsIndividual = $scope.dataset.fe_filters.fieldsCommaSeparatedAsIndividual.indexOf($scope.finalizedFieldName) != -1;
-
-                    // if (!$scope.dataset.fe_filters.fieldsMultiSelectable) $scope.dataset.fe_filters.fieldsMultiSelectable = [];
-                    // $scope.data.multipleSelection = $scope.dataset.fe_filters.fieldsMultiSelectable.indexOf($scope.finalizedFieldName) != -1;
-
-                    // if (!$scope.dataset.fe_filters.fieldsSortableByInteger) $scope.dataset.fe_filters.fieldsSortableByInteger = [];
-                    // $scope.data.sortableByInt = $scope.dataset.fe_filters.fieldsSortableByInteger.indexOf($scope.finalizedFieldName) != -1;
-
-                    // if (!$scope.dataset.fe_filters.fieldsSortableInReverseOrder) $scope.dataset.fe_filters.fieldsSortableInReverseOrder = [];
-                    // $scope.data.sortableInReverse = $scope.dataset.fe_filters.fieldsSortableInReverseOrder.indexOf($scope.finalizedFieldName) != -1;
-
-                    // if (!$scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName) $scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName = {};
-
-                    // if (!$scope.dataset.fe_filters.valuesToExcludeByOriginalKey) $scope.dataset.fe_filters.valuesToExcludeByOriginalKey = {};
-
-                    // if (!$scope.dataset.fe_filters.keywords) $scope.dataset.fe_filters.keywords = [];
-                    // $scope.data.keywords = $scope.dataset.fe_filters.keywords.find(function (elem) {
-                    //     return elem.title == $scope.finalizedFieldName;
-                    // });
-                    // if (!$scope.data.keywords) $scope.data.keywords = {title: $scope.finalizedFieldName, choices: []};
-
-                    // Nested
+                    }
                 };
 
                 $scope.reset();
@@ -121,7 +133,6 @@ angular.module('arraysApp')
                     } else {
                         field.push("");
                     }
-                    
                 }
 
                 $scope.DataTypeMatch = function(requireType) {
@@ -140,14 +151,27 @@ angular.module('arraysApp')
                         return true;
 
                     }
-                 
-
                 }
 
 
+                $scope.notChosen= function(arrayOfObject,target,index) {
+                    for (var i = 0; i <arrayOfObject.length ;i++) {
+                        if (index !== i) {
+                            if (arrayOfObject[i].key == target) {
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                }
 
 
-
+                $scope.checkDuplicateKey = function(list,$index) {
+                    return function(col) {
+                    
+                       return $scope.notChosen(list,col,$index);
+                    }
+                }
 
 
                 $scope.cancel = function () {
@@ -155,74 +179,10 @@ angular.module('arraysApp')
                 };
 
                 $scope.save = function () {
-                    // General
-
-                    console.log($scope.dataset);
-                    // if ($scope.data.designatedField != undefined)
-                    //     $scope.dataset.fe_designatedFields[$scope.data.designatedField] = $scope.finalizedFieldName;
-                    // else {
-                    //     for (var key in $scope.dataset.fe_designatedFields) {
-                    //         if ($scope.dataset.fe_designatedFields[key] == $scope.finalizedFieldName) {
-                    //             delete $scope.dataset[key];
-                    //             break;
-                    //         }
-                    //     }
-                    // }
-
-                    // var index = $scope.dataset.fe_fieldDisplayOrder.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.dataset.fe_fieldDisplayOrder.splice(index, 1);
-                    // if ($scope.data.displayOrder) {
-                    //     // TODO: Consider to shift the existing elements at the same position?
-                    //     $scope.dataset.fe_fieldDisplayOrder.splice($scope.data.displayOrder, 0, $scope.finalizedFieldName);
-                    // }
-
-                    // // Filter
-                    // index = $scope.dataset.fe_filters.fieldsNotAvailable.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.dataset.fe_filters.fieldsNotAvailable.splice(index, 1);
-                    // if ($scope.data.filterNotAvailable) {
-                    //     $scope.dataset.fe_filters.fieldsNotAvailable.push($scope.finalizedFieldName);
-                    // }
-
-                    // index = $scope.dataset.fe_filters.fieldsCommaSeparatedAsIndividual.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.dataset.fe_filters.fieldsCommaSeparatedAsIndividual.splice(index, 1);
-                    // if ($scope.data.commaSeparatedAsIndividual) {
-                    //     $scope.dataset.fe_filters.fieldsCommaSeparatedAsIndividual.push($scope.finalizedFieldName);
-                    // }
-
-                    // index = $scope.dataset.fe_filters.fieldsMultiSelectable.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.dataset.fe_filters.fieldsMultiSelectable.splice(index, 1);
-                    // if ($scope.data.multipleSelection) {
-                    //     $scope.dataset.fe_filters.fieldsMultiSelectable.push($scope.finalizedFieldName);
-                    // }
-
-                    // index = $scope.dataset.fe_filters.fieldsSortableByInteger.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.dataset.fe_filters.fieldsSortableByInteger.splice(index, 1);
-                    // if ($scope.data.sortableByInt) {
-                    //     $scope.dataset.fe_filters.fieldsSortableByInteger.push($scope.finalizedFieldName);
-                    // }
-
-                    // index = $scope.dataset.fe_filters.fieldsSortableInReverseOrder.indexOf($scope.finalizedFieldName);
-                    // if (index != -1) $scope.dataset.fe_filters.fieldsSortableInReverseOrder.splice(index, 1);
-                    // if ($scope.data.sortableInReverse) {
-                    //     $scope.dataset.fe_filters.fieldsSortableInReverseOrder.push($scope.finalizedFieldName);
-                    // }
-
-                    // if ($scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName[$scope.finalizedFieldName]) {
-                    //     $scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName[$scope.finalizedFieldName] =
-                    //         $scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName[$scope.finalizedFieldName].filter(function (elem) {
-                    //             return elem.value != '' || elem.override != '';
-                    //         });
-                    //     if ($scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName[$scope.finalizedFieldName].length == 0)
-                    //         delete $scope.dataset.fe_filters.oneToOneOverrideWithValuesByTitleByFieldName[$scope.finalizedFieldName];
-                    // }
-
-                    // $scope.dataset.fe_filters.keywords = $scope.dataset.fe_filters.keywords.filter(function (elem) {
-                    //     return elem.title != $scope.finalizedFieldName;
-                    // });
-                    // if ($scope.data.keywords.choices.length > 0) $scope.dataset.fe_filters.keywords = $scope.dataset.fe_filters.keywords.concat($scope.data.keywords);
-
-                    // Nested
-
+                    if ($scope.isDefault == true) {
+                        $scope.dataset.default_view = viewName;
+                    } 
+                    $scope.dataset.fe_views.views[viewName] = $scope.data;
                     $mdDialog.hide($scope.dataset);
                 };
             }
