c6bc9293bff7477e74999219c64e56000185ce39
Thomas Lee <lucky.thomaslee@gmail.com>
2016-12-02 09:17:54 +0800

    Fixed javascript issue on the dataset settings

--- public/javascripts/dashboard/controllers/dataset/settings.js
+++ public/javascripts/dashboard/controllers/dataset/settings.js
@@ -1,42 +1,38 @@
 angular.module('arraysApp')
-    .controller('DatasetSettingsCtrl', ['$scope', '$state', 'dataset', 'DatasetService', '$mdToast','FileUploader'
-        function($scope, $state, dataset, DatasetService, $mdToast,FileUploader) {
+    .controller('DatasetSettingsCtrl', ['$scope', '$state', 'dataset', 'DatasetService', '$mdToast', 'FileUploader',
+        function ($scope, $state, dataset, DatasetService, $mdToast, FileUploader) {
 
             $scope.$parent.$parent.dataset = dataset;
             $scope.$parent.$parent.currentNavItem = 'Settings';
 
 
-             var uploader = $scope.uploader = new FileUploader({
+            var uploader = $scope.uploader = new FileUploader({
                 url: 'upload.php'
             });
 
-        // FILTERS
+            // FILTERS
 
-        uploader.filters.push({
-            name: 'imageFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options) {
-                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-            }
-        });
-
-
-        
+            uploader.filters.push({
+                name: 'imageFilter',
+                fn: function (item /*{File|FileLikeObject}*/, options) {
+                    var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                    return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+                }
+            });
 
 
-            $scope.submitForm = function(isValid) {
+            $scope.submitForm = function (isValid) {
                 if (isValid) {
                     $scope.submitting = true;
                     if (!dataset.author) {
                         dataset.author = $scope.user._id;
                         dataset._team = $scope.user._team._id;
                         dataset.fe_displayTitleOverrides = {};
-                    } 
+                    }
                     dataset.updatedBy = $scope.user._id;
 
 
-
-                    DatasetService.save(dataset).then(function(id) {
+                    DatasetService.save(dataset).then(function (id) {
                         $mdToast.show(
                             $mdToast.simple()
                                 .textContent(dataset._id ? 'Dataset updated successfully!' : 'New Dataset was created successfully!')
@@ -44,9 +40,13 @@ angular.module('arraysApp')
                                 .hideDelay(3000)
                         );
 
-                        $state.transitionTo('dashboard.dataset.upload', {id: id}, { reload: true, inherit: false, notify: true });
+                        $state.transitionTo('dashboard.dataset.upload', {id: id}, {
+                            reload: true,
+                            inherit: false,
+                            notify: true
+                        });
                         $scope.submitting = false;
-                    }, function(error) {
+                    }, function (error) {
                         $mdToast.show(
                             $mdToast.simple()
                                 .textContent(error)
