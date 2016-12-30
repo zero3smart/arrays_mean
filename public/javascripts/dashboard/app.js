2442b3dc8128a4b9aeb27f29d06591a4cf204670
John Teske <john.teske@gmail.com>
2016-12-30 13:41:06 -0800

    Add basic f3b600 accent palette to Angular Material

--- public/javascripts/dashboard/app.js
+++ public/javascripts/dashboard/app.js
@@ -12,4 +12,24 @@ angular
         'mdColorPicker',
         'angularFileUpload',
         'ui.sortable'
-    ]);
+    ])
+    .config(["$mdThemingProvider", function($mdThemingProvider) {
+        $mdThemingProvider.definePalette('arraysPalette', {
+            '50': '#feb600',
+            '100': '#feb600',
+            '200': '#feb600',
+            '300': '#feb600',
+            '400': '#feb600',
+            '500': '#feb600',
+            '600': '#feb600',
+            '700': '#feb600',
+            '800': '#feb600',
+            '900': '#feb600',
+            'A100': '#feb600',
+            'A200': '#feb600',
+            'A400': '#feb600',
+            'A700': '#feb600'
+        });
+        $mdThemingProvider.theme('default')
+            .accentPalette('arraysPalette');
+    }]);
