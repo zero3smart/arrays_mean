5966d4c489f1c8e534403f1c82940efce8b97e11
John Teske <john.teske@gmail.com>
2016-12-31 14:51:38 -0800

    Add test data for plans and user

--- public/javascripts/dashboard/controllers/account/billing.js
+++ public/javascripts/dashboard/controllers/account/billing.js
@@ -1,6 +1,29 @@
 angular.module('arraysApp')
     .controller('BillingCtrl', ['$scope', '$mdDialog', function($scope, $mdDialog) {
 
+        // for testing
+        // name, cost per dataset
+        $scope.testPlans = {
+            trial: {
+                name: 'Trial',
+                cost: 0
+            },
+            pro: {
+                name: 'Pro',
+                cost: 149
+            },
+            // enterprise: {
+            //     name: 'Enterprise',
+            //     cost: 0
+            // }
+        };
+        // for testing, to attach to user
+        $scope.testUser = {};
+        $scope.testUser.plan = $scope.testPlans.pro;
+        $scope.testUser.billingCycle = 'Monthly';
+        $scope.testUser.paidDatasets = 2; // not the current number but the allowed, paid number
+        // for testing
+
         $scope.openBillingDialog = function(ev, template) {
             $mdDialog.show({
                 controller: BillingDialogController,
@@ -8,11 +31,18 @@ angular.module('arraysApp')
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 // clickOutsideToClose: true,
-                fullscreen: true
+                fullscreen: true,
+                locals: {
+                    testPlans: $scope.testPlans,
+                    testUser: $scope.testUser
+                }
             });
         };
 
-        function BillingDialogController($scope, $mdDialog) {
+        function BillingDialogController($scope, $mdDialog, testPlans, testUser) {
+            $scope.testPlans = testPlans;
+            $scope.testUser = testUser;
+
             $scope.hide = function() {
                 $mdDialog.hide();
             };
