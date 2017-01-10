f1a815c2d8dad3f42aa4d1496d01dd200ca6b187
John Teske <john.teske@gmail.com>
2017-01-09 18:13:40 -0800

    Add initial mdSidebar for dashboard
    Basic markup, functionality. To refine.

--- public/javascripts/dashboard/controllers/app.js
+++ public/javascripts/dashboard/controllers/app.js
@@ -1,7 +1,7 @@
 angular
     .module('arraysApp')
-    .controller('AdminCtrl', ['$scope', '$state', 'AuthService', '$window', '$location',
-        function ($scope, $state, AuthService, $window, $location) {
+    .controller('AdminCtrl', ['$scope', '$state', 'AuthService', '$window', '$location', '$mdSidenav',
+        function ($scope, $state, AuthService, $window, $location, $mdSidenav) {
 
             $scope.user = AuthService.currentUser();
             $scope.teams = AuthService.allTeams();
@@ -14,21 +14,29 @@ angular
             $scope.updateSubdomain();
 
             $scope.explore_url = $location.protocol() +  "://explore." +  $location.host() + ":" + $location.port();
-    
-            if (!isSmartDevice($window)) {
-                $scope.showSideMenu = true;
-            }
 
             $scope.logout = function() {
                 AuthService.logout();
             };
 
-            function isSmartDevice( $window )
-            {
-                // Adapted from http://www.detectmobilebrowsers.com
-                var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
-                // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
-                return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
+            $scope.toggleLeft = buildToggler('left');
+
+            function buildToggler(navID) {
+                return function() {
+                    $mdSidenav(navID).toggle()
+                }
             }
 
-        }]);
+            // if (!isSmartDevice($window)) {
+            //     $scope.showSideMenu = true;
+            // }
+
+            // function isSmartDevice( $window )
+            // {
+            //     // Adapted from http://www.detectmobilebrowsers.com
+            //     var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
+            //     // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
+            //     return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
+            // }
+
+    }]);
