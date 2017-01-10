921efb56300803a1747c072972dd8398e040d231
John Teske <john.teske@gmail.com>
2017-01-09 16:59:46 -0800

    Auto upload and submit team logos, mastheads, icons

--- public/javascripts/dashboard/controllers/website.js
+++ public/javascripts/dashboard/controllers/website.js
@@ -41,8 +41,11 @@ angular.module('arraysApp')
 
                         $scope.team[asset] = fileItem.uploadUrls[asset].publicUrl + '?' + new Date().getTime();
 
-                        if ($scope.vm[formName].$pristine) {
-                            $scope.vm[formName].$setDirty();
+                        if (this.formName) {
+                            if ($scope.vm[formName].$pristine) {
+                                $scope.vm[formName].$setDirty();
+                            }
+                            $scope.submitForm();
                         }
 
                         $mdToast.show(
@@ -55,22 +58,23 @@ angular.module('arraysApp')
                 }
 
                 _uploader.assetType = assetType;
+                _uploader.formName = formName;
                 return _uploader;
             }
 
-            $scope.iconsUploader = newUploader('icon', 'teamIconsForm');
+            $scope.iconsUploader = newUploader('icon');
 
             $scope.logoUploader = newUploader('logo', 'websiteForm');
 
             $scope.logo_headerUploader = newUploader('logo_header', 'websiteForm');
 
-            $scope.submitForm = function(formName, isValid) {
-                if (isValid) {
+            $scope.submitForm = function() {
+                if ($scope.vm.websiteForm.$valid && $scope.vm.websiteForm.$dirty) {
                     AuthService.updateTeam($scope.team)
                     .then(function(teams) {
                         $scope.$parent.teams = AuthService.allTeams();
                         $scope.$parent.team = AuthService.currentTeam();
-                        $scope.vm[formName].$setPristine();
+                        $scope.vm.websiteForm.$setPristine();
 
                         $mdToast.show(
                             $mdToast.simple()
