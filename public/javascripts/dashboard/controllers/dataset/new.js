a623b9c32da527eb45d03f6f5e9a3f51540c761d
John Teske <john.teske@gmail.com>
2017-01-16 15:43:47 -0800

    Remove new dataset toast and tweak language

--- public/javascripts/dashboard/controllers/dataset/new.js
+++ public/javascripts/dashboard/controllers/dataset/new.js
@@ -31,12 +31,12 @@ angular.module('arraysApp')
                     DatasetService.save(dataset).then(function (response) {
 
                        if (response.status == 200) {
-                            $mdToast.show(
-                                $mdToast.simple()
-                                    .textContent(dataset._id ? 'Dataset updated successfully!' : 'New Dataset was created successfully!')
-                                    .position('top right')
-                                    .hideDelay(3000)
-                            );
+                            // $mdToast.show(
+                            //     $mdToast.simple()
+                            //         .textContent(dataset._id ? 'Dataset updated successfully!' : 'New Dataset was created successfully!')
+                            //         .position('top right')
+                            //         .hideDelay(3000)
+                            // );
 
                             $state.transitionTo('dashboard.dataset.upload', {id: response.data.id}, {
                                 reload: true,
