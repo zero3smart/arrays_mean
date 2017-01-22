8e36c96282c8d5397036951e2f266d7019b38522
John Teske <john.teske@gmail.com>
2017-01-21 20:07:24 -0800

    Add aaDialogActions directive
    For consistent styling of dashboard dialog controls

--- public/javascripts/dashboard/directives/aa-toolbar.js
+++ public/javascripts/dashboard/directives/aa-toolbar.js
@@ -8,11 +8,22 @@ angular.module('arraysApp')
             templateUrl: 'templates/blocks/dialog.toolbar.html'
         };
     })
-    .directive("aaTooltip", function() {
-      return {
-        scope: { aaTooltip: '@'},
-        restrict: 'A',
-        transclude: true,
-        template: '<span ng-transclude></span><md-tooltip md-delay="600">{{aaTooltip}}</md-tooltip>'
-      };
+    .directive('aaDialogActions', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                confirm: '@confirm',
+                warn: '@warn',
+                cancel: '@cancel'
+            },
+            templateUrl: 'templates/blocks/dialog.actions.html'
+        };
+    })
+    .directive('aaTooltip', function() {
+        return {
+            scope: { aaTooltip: '@'},
+            restrict: 'A',
+            transclude: true,
+            template: '<span ng-transclude></span><md-tooltip md-delay="600">{{aaTooltip}}</md-tooltip>'
+        };
     });
