ecee77f86d1afb68c8bf708a552dcfc6e92fbc2f
John Teske <john.teske@gmail.com>
2017-01-24 14:21:46 -0800

    Add default-sort-order directive

--- public/javascripts/dashboard/directives/aa-format-views.js
+++ public/javascripts/dashboard/directives/aa-format-views.js
@@ -1,9 +1,18 @@
 angular.module('arraysApp')
+    .directive('aaViewDefaultSortOrder', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                // title: '@title'
+            },
+            templateUrl: 'templates/blocks/views/default-sort-order.html'
+        };
+    })
     .directive('aaViewConfigMenus', function() {
         return {
             restrict: 'E',
             scope: {
-                title: '@title'
+                // title: '@title'
             },
             templateUrl: 'templates/blocks/views/config-menus.html'
         };
