320f3e13c50aab0f8a958239e3039f6b7c2e4258
John Teske <john.teske@gmail.com>
2017-02-03 12:10:42 -0800

    Add conditional language for 0,1,inf fields available in menu
    Began button to take user back to set a field but will require more
    logic to save or cancel before nagivating away.

--- public/javascripts/dashboard/controllers/dataset/views.js
+++ public/javascripts/dashboard/controllers/dataset/views.js
@@ -352,6 +352,19 @@ angular.module('arraysApp')
 
                 // $scope.data.default_view = default_view;
 
+                // save or cancel before navigating back to include a field
+                // $scope.goToData = function() {
+                //     if ($scope.viewForm.$dirty || $scope.viewForm.$valid) {
+                //         // $scope.save();
+                //     } else {
+                //         // $scope.cancel();
+                //     }
+                //     $state.transitionTo('dashboard.dataset.data', {id: dataset._id}, {
+                //         reload: true,
+                //         inherit: false,
+                //         notify: true
+                //     });
+                // };
 
                 $scope.addMore = function (field,pushType) {
                     if (pushType == 'object') {
