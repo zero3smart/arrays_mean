6b27018e304ad1636ad980801f5fd8c95307045d
pruhter9 <ruhter.philip@gmail.com>
2017-02-21 12:57:45 -0800

    scrollable header for more views

--- public/javascripts/linechart/main.js
+++ public/javascripts/linechart/main.js
@@ -2,3 +2,35 @@
  * Define linechart common name space.
  */
 var linechart = {};
+
+$(function () {
+    /**
+     * Initialize controller
+     */
+    var controller = new ScrollMagic.Controller();
+
+    /**
+     * Set array header pin
+     */
+
+     if (window.location.href.indexOf('embed=true') > -1) {
+         var scene = new ScrollMagic.Scene({
+                offset: 0,
+                triggerElement: '#array-controls'
+            })
+            .triggerHook('onLeave')
+            .setPin('#array-controls')
+            .setClassToggle('body', 'array-controls-pinned')
+            .addTo(controller);
+     }
+     else {
+         var scene = new ScrollMagic.Scene({
+                offset: - $('.navbar-fixed-top').innerHeight(),
+                triggerElement: '#array-controls'
+            })
+            .triggerHook('onLeave')
+            .setPin('#array-controls')
+            .setClassToggle('body', 'array-controls-pinned')
+            .addTo(controller);
+     }
+});
