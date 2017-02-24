e85c61e6d11e99a6f4e912c60aa183afd8d68a73
John Teske <john.teske@gmail.com>
2017-02-24 12:28:02 -0800

    Lint dataset.js

--- public/javascripts/dashboard/controllers/dataset.js
+++ public/javascripts/dashboard/controllers/dataset.js
@@ -1,6 +1,6 @@
 angular.module('arraysApp')
     .controller('DatasetCtrl', ['$scope', '$location', '$state', '$rootScope','DatasetService','$q',
-        function($scope, $location, $state, $rootScope,DatasetService,$q) {
+        function($scope, $location, $state, $rootScope, DatasetService, $q) {
 
             $scope.currentStep = $state.current.name;
 
@@ -17,24 +17,18 @@ angular.module('arraysApp')
 
             $scope.navigate = function(step, anchor) {
                 var errorHandler = function (error) {
-                        $mdToast.show(
+                    $mdToast.show(
                         $mdToast.simple()
                             .textContent(error)
                             .position('top right')
                             .hideDelay(5000)
                     );
-                }
+                };
 
                 var done = function() {
 
                     $scope.currentStep = step;
                     switch (step) {
-                    case 'dashboard.dataset.settings':
-                        if ($scope.dataset._id) {
-                            $location.hash(anchor || '');
-                            $location.path('/dashboard/dataset/settings/' + $scope.dataset._id);
-                        }
-                        break;
                     case 'dashboard.dataset.upload':
                         if ($scope.dataset._id) {
                             $location.path('/dashboard/dataset/upload/' + $scope.dataset._id);
@@ -49,68 +43,62 @@ angular.module('arraysApp')
                     case 'dashboard.dataset.views':
                         $location.path('/dashboard/dataset/views/' + $scope.dataset._id);
                         break;
+                    case 'dashboard.dataset.settings':
+                        if ($scope.dataset._id) {
+                            $location.hash(anchor || '');
+                            $location.path('/dashboard/dataset/settings/' + $scope.dataset._id);
+                        }
+                        break;
                     case 'dashboard.dataset.done':
                         if ( ($scope.dataset.fe_listed && $scope.dataset.fe_visible && $scope.dataset.fe_views.default_view )|| (!$scope.dataset.fe_listed && !$scope.dataset.fe_visible) ) {
                             $location.path('/dashboard/dataset/done/' + $scope.dataset._id);
                         }
                         break;
                     }
-                }
-
-
+                };
 
                 var queue = [];
 
-                    var finalizedDataset = angular.copy($scope.dataset);
-                    delete finalizedDataset.columns;
-                    delete finalizedDataset.__v;
+                var finalizedDataset = angular.copy($scope.dataset);
+                delete finalizedDataset.columns;
+                delete finalizedDataset.__v;
 
-                    queue.push(DatasetService.save(finalizedDataset));
+                queue.push(DatasetService.save(finalizedDataset));
 
-                   if ($scope.additionalDatasources) {
+                if ($scope.additionalDatasources) {
+                    $scope.additionalDatasources.forEach(function(datasource) {
+                        var finalizedDatasource = angular.copy(datasource);
 
-                        $scope.additionalDatasources.forEach(function(datasource) {
-                            var finalizedDatasource = angular.copy(datasource);
-                            delete finalizedDatasource.fn_new_rowPrimaryKeyFromRowObject;
-                            delete finalizedDatasource.raw_rowObjects_coercionScheme;
-                            delete finalizedDatasource._otherSources;
-                            delete finalizedDatasource._team;
-                            delete finalizedDatasource.title;
-                            delete finalizedDatasource.__v;
-                            delete finalizedDatasource.importRevision;
-                            delete finalizedDatasource.author;
-                            delete finalizedDatasource.updatedBy;
-                            delete finalizedDatasource.brandColor;
-                            delete finalizedDatasource.customFieldsToProcess;
-                            delete finalizedDatasource.urls;
-                            delete finalizedDatasource.description;
-                            delete finalizedDatasource.fe_designatedFields;
-                            delete finalizedDatasource.fe_excludeFields;
-                            delete finalizedDatasource.fe_displayTitleOverrides;
-                            delete finalizedDatasource.fe_fieldDisplayOrder;
-                            delete finalizedDatasource.imageScraping;
-                            delete finalizedDatasource.isPublic;
-                            delete finalizedDatasource.fe_views;
-                            delete finalizedDatasource.fe_filters;
-                            delete finalizedDatasource.fe_objectShow_customHTMLOverrideFnsByColumnNames;
+                        delete finalizedDatasource.fn_new_rowPrimaryKeyFromRowObject;
+                        delete finalizedDatasource.raw_rowObjects_coercionScheme;
+                        delete finalizedDatasource._otherSources;
+                        delete finalizedDatasource._team;
+                        delete finalizedDatasource.title;
+                        delete finalizedDatasource.__v;
+                        delete finalizedDatasource.importRevision;
+                        delete finalizedDatasource.author;
+                        delete finalizedDatasource.updatedBy;
+                        delete finalizedDatasource.brandColor;
+                        delete finalizedDatasource.customFieldsToProcess;
+                        delete finalizedDatasource.urls;
+                        delete finalizedDatasource.description;
+                        delete finalizedDatasource.fe_designatedFields;
+                        delete finalizedDatasource.fe_excludeFields;
+                        delete finalizedDatasource.fe_displayTitleOverrides;
+                        delete finalizedDatasource.fe_fieldDisplayOrder;
+                        delete finalizedDatasource.imageScraping;
+                        delete finalizedDatasource.isPublic;
+                        delete finalizedDatasource.fe_views;
+                        delete finalizedDatasource.fe_filters;
+                        delete finalizedDatasource.fe_objectShow_customHTMLOverrideFnsByColumnNames;
 
+                        queue.push(DatasetService.save(finalizedDatasource));
+                    });
+                }
 
-
-                            queue.push(DatasetService.save(finalizedDatasource));
-                        });
-
-
-
-
-                   }
-
-
-
-                    $q.all(queue)
-                        .then(done)
-                        .catch(errorHandler);
-
-
+                $q.all(queue)
+                    .then(done)
+                    .catch(errorHandler);
             };
 
             $scope.convertToURLSafe = function(input) {
