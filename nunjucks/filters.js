302255eaaa827bd927a799045c487dcb9bcff993
Maitland McConnell <maitlandm@gmail.com>
2017-03-01 15:29:16 -0800

    fixes tiles

--- nunjucks/filters.js
+++ nunjucks/filters.js
@@ -9,9 +9,6 @@ module.exports = function (nunjucks_env,env) {
 
     nunjucks_env.addFilter('comma', commaFilter);
     // General/shared
-    nunjucks_env.addFilter('dateFormattedAs_monthDayYear', function (date) {
-        return moment(date).utc().format("MMMM Do, YYYY");
-    });
     nunjucks_env.addFilter('dateFormattedAs_monthDayYear_array', function (date) {
         return moment(date).utc().format("MMM D, YYYY");
     });
