670655eb6fedb53165b3c163dde17385dbc50442
Andre Lindquist <andrelindquist@gmail.com>
2017-03-02 16:22:32 -0800

    Update nodemailer.js

--- app/libs/utils/nodemailer.js
+++ app/libs/utils/nodemailer.js
@@ -54,9 +54,7 @@ module.exports.sendVizFinishProcessingEmail = function(user,dataset,team,cb) {
 
 	datasetLink += team.subdomain + '.' + rootDomain + '/' + dataset.uid + '-r' + dataset.importRevision + '/' + default_view;
 
-	var htmlText = 'Hi, ' + user.firstName + '<br> This email is to inform you that the dataset "' + dataset.title + '" that you ' +
-	'initiatated the import ' + 
-	'has been finished. You can view your dataset here: ' +  datasetLink + '<br><br><br> ArraysTeam';
+	var htmlText = 'Hi, ' + user.firstName + '<br> This email is to inform you that the dataset, "' + dataset.title + '" has finished importing. <br>You can view your dataset here: ' +  datasetLink + '<br><br><br> ArraysTeam';
 	var mailOptions = {
 		from : 'info@arrays.co',
 		to: user.email,
@@ -71,7 +69,7 @@ module.exports.sendVizFinishProcessingEmail = function(user,dataset,team,cb) {
 
 function sendVizDisplayStatusUpdate(state,authorName,authorEmail,datasetTitle,cb) {
 	var sub = '[Dataset Display Status]: ' + state;
-	var htmlText = 'Hi, ' + authorName + '<br>Your dataset (title:' + datasetTitle + ') is ' + state + ' to be listed on arrays.co.';
+	var htmlText = 'Hi, ' + authorName + '<br>Your dataset (title:' + datasetTitle + ') has been ' + state + ' for listing on arrays.co.';
 	var mailOptions = {
 		from: 'info@arrays.co', 
 		to: authorEmail,
