d1da219128450998a5a41b4cb13842e26bd3e8f1
Andre Lindquist <andrelindquist@gmail.com>
2017-03-21 00:48:43 -0700

    Make terms of service checkbox required

--- public/javascripts/signup/signup.dir.js
+++ public/javascripts/signup/signup.dir.js
@@ -193,4 +193,18 @@
 
 	}])
 
+	signupModule.directive('checkRequired', function(){
+	  return {
+	    require: 'ngModel',
+	    restrict: 'A',
+	    link: function (scope, element, attrs, ngModel) {
+	      ngModel.$validators.checkRequired = function (modelValue, viewValue) {
+	        var value = modelValue || viewValue;
+	        var match = scope.$eval(attrs.ngTrueValue) || true;
+	        return value && match === value;
+	      }
+	    }
+	  } 
+	})
+
 })();
