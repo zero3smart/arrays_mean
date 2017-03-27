24da34209f6994a52877cb232e06d4902537d512
Maitland McConnell <maitlandm@gmail.com>
2017-03-27 12:31:56 -0700

    fixes number of items aggregate

--- app/controllers/client/data_preparation/map.js
+++ app/controllers/client/data_preparation/map.js
@@ -104,6 +104,9 @@ module.exports.BindData = function (req, urlQuery, callback) {
             var aggregateBy_realColumnName = aggregateBy ? importedDataPreparation.RealColumnNameFromHumanReadableColumnName(aggregateBy, dataSourceDescription) :
             (typeof dataSourceDescription.fe_views.views.map.defaultAggregateByColumnName  == 'undefined') ? importedDataPreparation.RealColumnNameFromHumanReadableColumnName(defaultAggregateByColumnName_humanReadable,dataSourceDescription) :
             dataSourceDescription.fe_views.views.map.defaultAggregateByColumnName;
+            if (!defaultAggregateByColumnName_humanReadable) {
+                defaultAggregateByColumnName_humanReadable = config.aggregateByDefaultColumnName;
+            }
 
             var sourceDoc, sampleDoc, uniqueFieldValuesByFieldName, mapFeatures = [], highestValue = 0, coordFeatures = [], coordMinMax = {min: 0, max: 0}, coordRadiusValue, coordTitle;
             var latField = dataSourceDescription.fe_views.views.map.latitudeField,
